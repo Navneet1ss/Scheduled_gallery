ac5db0691fb6d9ad3478aaa2331fcfa1772b91ef
Navneet Bhargava <nikbhargava19@gmail.com>
2022-02-16 20:50:32 +0530

    first commit

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,5 @@ module GalleryApp
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.active_storage.replace_on_assign_to_many = false
-    config.assets.initialize_on_precompile = false
   end
 end
